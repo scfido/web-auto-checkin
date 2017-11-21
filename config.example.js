9e0438ebfe18ce5b2fa3f41686b694e29badc023
fuyun <scfido@gmail.com>
2017-11-21 10:55:47 +0800

    示例配置，用数组保存多站点

--- config.example.js
+++ config.example.js
@@ -1,18 +1,18 @@
 
 module.exports = {
-    sites: {
-        "abc": {
+    sites: [
+        {
             loginUrl: "https://abc.com/auth/login",
             username: "yourname@mail.com",
             password: "yourpassword",
             title: "登陆后的网页标题",
-            disabled: true            
+            disabled: true
         },
-        "def": {
+        {
             loginUrl: "http://def.com/auth/login",
             username: "yourname@mail.com",
             password: "yourpassword",
             title: "登陆后的网页标题"
         }
-    }
+    ]
 }
